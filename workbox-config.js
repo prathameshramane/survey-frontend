664d1ce2b443fc3ea54a2255329050761f5b736c
Prathamesh Ramane <ppramane@gmail.com>
2022-09-10 20:15:03 +0530

    Basic PWA

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "dist/angular-workbox/",
+  "globPatterns": [
+    "**/*.{txt,png,ico,html,js,json,css}"
+  ],
+  "swDest": "dist/angular-workbox/sw-default.js",
+  "globIgnores": [
+    
+    "3rdpartylicenses.txt"
+  ]
+};
