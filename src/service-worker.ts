b20ee1cea5fa7bcc1210aa53723254a9e736fbeb
Prathamesh Ramane <ppramane@gmail.com>
2022-09-13 21:59:00 +0530

    Complete New Implementation

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,5 +1,41 @@
-import {precacheAndRoute} from 'workbox-precaching';
+import * as workboxCore from 'workbox-core';
+import * as workboxPrecaching from 'workbox-precaching';
+import * as workboxRouting from 'workbox-routing';
+import * as workboxStrategy from 'workbox-strategies';
+import * as workboxExpiration from 'workbox-expiration';
+import * as workboxBackgroundSync from 'workbox-background-sync';
 
 declare const self: ServiceWorkerGlobalScope;
 
-precacheAndRoute(self.__WB_MANIFEST);
+// Modify SW update cycle
+workboxCore.clientsClaim();
+workboxCore.skipWaiting();
+
+workboxPrecaching.precacheAndRoute(self.__WB_MANIFEST);
+
+// Runtime caching
+workboxRouting.registerRoute(
+    new RegExp('https://jsonplaceholder.typicode.com/(.*)'),
+    new workboxStrategy.StaleWhileRevalidate({
+        cacheName:'jsonplaceholder',
+        plugins:[
+            new workboxExpiration.ExpirationPlugin({
+                maxEntries:30,
+                maxAgeSeconds: 60 * 60 // 1hr
+            })
+        ]
+    })
+)
+
+// Background Sync
+workboxRouting.registerRoute(
+    new RegExp('https://jsonplaceholder.typicode.com/(.*)'),
+    new workboxStrategy.NetworkOnly({
+        plugins:[
+            new workboxBackgroundSync.BackgroundSyncPlugin('jsonQueue', {
+                maxRetentionTime: 24 * 60 // Retry max of 24hrs
+            })
+        ]
+    }),
+    'POST'
+)
